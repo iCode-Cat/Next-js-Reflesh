1689c40203fb103321f8c9a734243bc600109ba0
iCode-Cat <devranboyac8@gmail.com>
2021-07-29 22:12:44 +0300

    Access Router Name

--- next.js-reflesh/pages/news/[id].js
+++ next.js-reflesh/pages/news/[id].js
@@ -1,6 +1,9 @@
 // /news/id
-
+import { useRouter } from 'next/router';
 const Detail = () => {
+  const router = useRouter();
+
+  console.log(router.query.id);
   return <div>NEWS DETAIL</div>;
 };
 
